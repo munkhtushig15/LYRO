c377155fc0f2b0e9b3dc65f31309828fb16a7581
Munkhtushig <munkhtushig@orgils-iMac.local>
2023-04-04 18:46:23 +0800

    browser router oruulsaan

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ front/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
