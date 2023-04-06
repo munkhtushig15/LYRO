587c7df8f29448eb1153b9f1c98722b9aa49788f
Munkhtushig <munkhtushig@orgils-iMac.local>
2023-04-06 19:38:26 +0800

    header

--- front/src/comps/Header.jsx
+++ front/src/comps/Header.jsx
@@ -4,16 +4,54 @@ const Header = () => {
   return (
     <div className="headerContainer">
       <div className="headerTop">
-        <span>SignUp</span>
-        <span>Login</span>
+        <div className="arrows">
+          <i class="gg-arrow-right-r"></i>
+          <span>Sign Up</span>
+        </div>
+        <div className="arrows">
+          <i class="gg-arrow-right-r"></i>
+          <span>Login</span>
+        </div>
       </div>
       <div className="headerLogoContainer">
-        <img id="pageLogo" src={require("../images/LYROLogo.png")} alt="@-@" />
-        <span id="pageLogoText">
-            LYRO
-        </span>
+        <div className="topLogoBox">
+          <div className="topLogo">
+            <img
+              id="pageLogo"
+              src={require("../images/logo.png")}
+              alt="@-@"
+            />
+            <span id="pageLogoText">LYRO</span>
+          </div>
+          <span className="bottomLogo">Pinecone Demo Day</span>
+        </div>
+        <div className="searchLine">
+          <input type="text" placeholder="Search ..." />
+          <button className="searchButton">Search</button>
+        </div>
       </div>
-      <div></div>
+      <div className="cateContainer">
+        <div className="cateType">
+          <i class="gg-arrow-down-o"></i>
+          <span>Mountain</span>
+        </div>
+        <div className="cateType">
+          <i class="gg-arrow-down-o"></i>
+          <span>Beach</span>
+        </div>
+        <div className="cateType">
+          <i class="gg-arrow-down-o"></i>
+          <span>City</span>
+        </div>
+        <div className="cateType">
+          <i class="gg-arrow-down-o"></i>
+          <span>Forest</span>
+        </div>
+        <div className="cateType">
+          <i class="gg-arrow-down-o"></i>
+          <span>Land</span>
+        </div>
+      </div>
     </div>
   );
 };
