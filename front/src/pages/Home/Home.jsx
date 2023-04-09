28dcce9f46837f8cbd275b405ebf67c946612a89
WiecznyX <70148164+munkhtushig15@users.noreply.github.com>
2023-04-09 21:36:48 +0800

    header

---
+++ front/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import "./Home.css";
+import Header from "../../comps/Header";
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <Header />
+      <main>
+        <div className="contents">
+          <p className="title">LATEST ISSUE</p>
+          
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
