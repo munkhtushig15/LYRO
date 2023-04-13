ed841028bc9a87510ef4cbe7fd9ffce89951fc5f
Munkhtushig <munkhtushig@orgils-iMac.local>
2023-04-13 18:11:10 +0800

    Big ads

--- FrontEnd/src/comps/BigAds.jsx
+++ FrontEnd/src/comps/BigAds.jsx
@@ -1,9 +1,98 @@
 import "../App.css";
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const BigAds = () => {
-  return <div className="bigAdsContainer">
-    
-  </div>;
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads2.jpg")}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads3.jpg")}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads4.jpg")}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads7.jpg")}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads6.jpg")}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className=" Ads"
+          src={require("../images/Ads5.jpg")}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default BigAds;
