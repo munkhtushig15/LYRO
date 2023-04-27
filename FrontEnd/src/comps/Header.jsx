4e13db6a9a7b4cd1a527594096d893d7340bf9e7
2022HOPE-BoldErdene <2022hope-bolderdene@27s-iMac.local>
2023-04-27 16:19:00 +0800

    Little bit changes

--- FrontEnd/src/comps/Header.jsx
+++ FrontEnd/src/comps/Header.jsx
@@ -1,39 +1,59 @@
 import "../App.css";
 import { Link } from "react-router-dom";
+import { instance } from "../App";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 const Header = () => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [isName, setIsName] = useState(false);
+  const id = JSON.parse(localStorage.getItem("user_id"));
+  const getUser = async () => {
+    const res = await instance.get(`/users/${id}`);
+    console.log(res);
+    setName(res.data.data.nickName);
+    setAge(res.data.data.age);
+    setIsName(true);
+  };
+  useEffect(() => {
+    getUser();
+  });
   return (
     <div className="headerContainer">
       <div className="headerTop">
-        <div className="arrows">
-          <i class="gg-arrow-right-r"></i>
+        {isName ? (
+          <Button
+            style={{
+              color: "white",
+              fontFamily: "Roboto Condensed, sans-serif",
+            }}
+          >
+            <div className="arrows">
+              <i className="gg-profile"></i>
+
+              {name}
+              {age}
+            </div>
+          </Button>
+        ) : (
           <Link
-            to="/SignUp"
+            to="/Login"
             style={{
               textDecoration: "none",
               color: "white",
               fontFamily: "Roboto Condensed, sans-serif",
             }}
           >
-            <span>Sign Up</span>
+            <div className="arrows">
+              <i className="gg-arrow-right-r"></i>
+              Login
+            </div>
           </Link>
-        </div>
-        <Link
-          to="/Login"
-          style={{
-            textDecoration: "none",
-            color: "white",
-            fontFamily: "Roboto Condensed, sans-serif",
-          }}
-        >
-          <div className="arrows">
-            <i class="gg-arrow-right-r"></i>
-            Login
-          </div>
-        </Link>
+        )}
       </div>
       <div className="headerLogoContainer">
         <Link
-          to="/"
+          to="/Home"
           style={{
             textDecoration: "none",
             color: "white",
@@ -56,30 +76,30 @@ const Header = () => {
           <Link to={"/Search"} className="searchLine">
             <input type="text" placeholder="Search ..." />
             <button className="searchButton">
-              <i class="gg-search searchButton"></i>
+              <i className="gg-search searchButton"></i>
             </button>
           </Link>
         </div>
       </div>
       <div className="cateContainer">
         <div className="cateType">
-          <i class="gg-arrow-down-o"></i>
+          <i className="gg-arrow-down-o"></i>
           <span>Mountain</span>
         </div>
         <div className="cateType">
-          <i class="gg-arrow-down-o"></i>
+          <i className="gg-arrow-down-o"></i>
           <span>Breach</span>
         </div>
         <div className="cateType">
-          <i class="gg-arrow-down-o"></i>
+          <i className="gg-arrow-down-o"></i>
           <span>City</span>
         </div>
         <div className="cateType">
-          <i class="gg-arrow-down-o"></i>
+          <i className="gg-arrow-down-o"></i>
           <span>Forest</span>
         </div>
         <div className="cateType">
-          <i class="gg-arrow-down-o"></i>
+          <i className="gg-arrow-down-o"></i>
           <span>Land</span>
         </div>
       </div>
