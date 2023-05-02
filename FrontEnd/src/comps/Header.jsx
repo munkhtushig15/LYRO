9f37d2ace78da2ff8b747d5ed576bcb3a2aa6a42
Munkhtushig <munkhtushig@orgils-iMac.local>
2023-05-02 17:18:32 +0800

    footer

--- FrontEnd/src/comps/Header.jsx
+++ FrontEnd/src/comps/Header.jsx
@@ -223,7 +223,7 @@ const Header = () => {
               />
             </div>
           </div>
-          <div className="logout">Logout</div>
+          <div className="logout" onClick={logOut}>Logout</div>
         </div>
       )}
     </>
