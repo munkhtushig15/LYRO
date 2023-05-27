3a24f52cccc94af6121df719f35e24c67230e977
BRPS12 <boochkoob@gmail.com>
2023-05-27 16:11:43 +0800

    bakcn

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -14,13 +14,6 @@ app.use((req, res ,next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 })
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-  });
 
 app.use(cors());
 app.use("/users", userRouter);
